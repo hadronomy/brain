cd3146efc2ac93756ebcbe671be3e23b82a8d2de
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-09-17 00:50:10 +0100

    feat: use tabler icon for search

--- quartz/components/Search.tsx
+++ quartz/components/Search.tsx
@@ -24,8 +24,9 @@ export default ((userOpts?: Partial<SearchOptions>) => {
           <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7">
             <title>Search</title>
             <g class="search-path" fill="none">
-              <path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4" />
-              <circle cx="8" cy="8" r="7" />
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+              <path d="M21 21l-6 -6" />
             </g>
           </svg>
         </button>
